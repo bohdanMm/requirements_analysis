034b9f58edf544b0f334ae274eb18762d7eb3a82
Rostyslav_Harbar <rostyslavgarbar@gmail.com>
2020-12-26 13:23:09 +0200

    Fix create method

--- src/main/java/nulp/architecture/service/MainUserService.java
+++ src/main/java/nulp/architecture/service/MainUserService.java
@@ -6,15 +6,18 @@ import nulp.architecture.domain.dto.MainUserRequest;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class MainUserService {
+    UserRepository userRepository;
+    UserMapper userMapper;
 
-    public MainUser create(MainUserRequest mainUserRequest) {
+    public MainUserResponse create(MainUserRequest mainUserRequest) {
         MainUser mainUser = MainUserBuilder.builder()
                 .email(mainUserRequest.getEmail())
                 .password(mainUserRequest.getPassword())
                 .phoneNumber(mainUserRequest.getPhoneNumber())
                 .build();
-
+        return userMapper.toResponseDto(userRepository.save(mainUser));
     }
 
     public MainUserResponse login(MainUserLogin mainUserLogin) {
